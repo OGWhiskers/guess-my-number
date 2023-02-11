026940baff4500eebbdca47e45535ab14c6d8c3f
OGWhiskers <bayandadlamini02@gmail.com>
2023-02-11 15:38:02 +0200

    Add event listener to check button
    Event listener checks wither number guesses is lower, higher or the same
    as the random number generated. Then returns string in message class.

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+"use strict";
 
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
+
+document.querySelector(".check").addEventListener("click", function () {
+  let guess = Number(document.querySelector(".guess").value);
+
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "No Number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Great guess !!!";
+  } else if (guess > secretNumber) {
+    document.querySelector(".message").textContent = "Too High !!!";
+  } else if (guess < secretNumber) {
+    document.querySelector(".message").textContent = "Too Low !!!";
+  }
+});
