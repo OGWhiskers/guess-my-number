23698bd2d291991dc5d7722d2e6229f3067733ce
OGWhiskers <bayandadlamini02@gmail.com>
2023-02-11 17:51:48 +0200

    Add win conditions for right guess

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 let score = 20;
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-console.log(secretNumber);
+document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
   let guess = Number(document.querySelector(".guess").value);
@@ -11,16 +11,24 @@ document.querySelector(".check").addEventListener("click", function () {
 
   if (!guess) {
     document.querySelector(".message").textContent = "No Number!";
-  } else if (guess === secretNumber) {
+  }
+  // Wins game
+  else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "Great guess !!!";
     document.querySelector(".score").textContent = score;
-  } else if (guess > secretNumber) {
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector("body").style.backgroundColor = "#2ff20c";
+  }
+  // Guess too high
+  else if (guess > secretNumber) {
     if (score > 0) {
       score--;
       document.querySelector(".message").textContent = "Too High !!!";
       document.querySelector(".score").textContent = score;
     }
-  } else if (guess < secretNumber) {
+  }
+  // Guess too low
+  else if (guess < secretNumber) {
     if (score > 0) {
       score--;
       document.querySelector(".message").textContent = "Too Low !!!";
