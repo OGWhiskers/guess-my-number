8429a6ec5be0c0306feda8a06d4ea8a81c0f509b
OGWhiskers <bayandadlamini02@gmail.com>
2023-02-11 16:09:18 +0200

    Add score feature

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+let score = 20;
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(secretNumber);
 
@@ -12,9 +13,18 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".message").textContent = "No Number!";
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "Great guess !!!";
+    document.querySelector(".score").textContent = score;
   } else if (guess > secretNumber) {
-    document.querySelector(".message").textContent = "Too High !!!";
+    if (score > 0) {
+      score--;
+      document.querySelector(".message").textContent = "Too High !!!";
+      document.querySelector(".score").textContent = score;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector(".message").textContent = "Too Low !!!";
+    if (score > 0) {
+      score--;
+      document.querySelector(".message").textContent = "Too Low !!!";
+      document.querySelector(".score").textContent = score;
+    }
   }
 });
